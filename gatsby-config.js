e64f668129a64db727394c4c88e0febcfc091e29
jasalguero <jose@aplos.com>
2020-05-28 00:54:04 +0200

    added analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-3549428-1`,
       },
     },
     `gatsby-plugin-feed`,
